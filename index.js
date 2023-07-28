042abb4979e5166826ada526c46634489958cf84
Ckauark <ckauark@gmail.com>
2023-07-28 14:21:21 -0300

    feat: get /update-cobj and get /

--- index.js
+++ index.js
@@ -8,19 +8,58 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-c0a3dd8b-b2c3-44d4-8ad4-e4a7cb6a07d4';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 
+app.get('/', async (req, res) => {
+  const cars = 'https://api.hubspot.com/crm/v3/objects/car';
+  const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+  }
+  try {
+      const resp = await axios.get(cars, { headers });
+      const data = resp.data.results;
+      console.log('logdata', resp.data.results);
+      res.render('homepage', { title: 'Custom Object Table | Cars', data });      
+  } catch (error) {
+      console.error(error);
+  }
+});
+
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
 
+app.get('/update-cobj', async (req, res) => {
+  try {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
+  } catch (error) {
+      console.error(error);
+  }
+});
+
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+app.post('/update-cobj', async (req, res) => {
+  const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+  const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+  }
+  try {
+      const resp = await axios.get(contacts, { headers });
+      const data = resp.data.results;
+      res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
+  } catch (error) {
+      console.error(error);
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
